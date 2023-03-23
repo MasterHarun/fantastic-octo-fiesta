211165e51459556c7434f27ebb51b4a7fdd8958e
Master <master.harun@gmail.com>
2023-03-23 00:44:27 -0400

    Refactored interaction method. Added new reset command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use serenity::{
   async_trait,
   http::Http,
   model::{
+    application::interaction::application_command::ApplicationCommandInteraction,
     gateway::Ready,
     id::{ChannelId, UserId},
     prelude::{
@@ -57,99 +58,145 @@ impl EventHandler for Handler {
   // Event handler for when an interaction is created
   async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
     if let Interaction::ApplicationCommand(command) = interaction {
-      if command.data.name == "chat" {
-        let input = command
-          .data
-          .options
-          .get(0)
-          .and_then(|opt| opt.value.as_ref())
-          .and_then(|value| value.as_str())
-          .unwrap_or("");
+      // Acknowledge the interaction
+      if let Err(_) = acknowledge_interaction(&command, &ctx).await {
+        return;
+      }
 
-        // Acknowledge the interaction within 3 seconds
-        let acknowledge_result = timeout(
-          Duration::from_millis(2500),
-          command.create_interaction_response(&ctx.http, |response| {
-            response.kind(InteractionResponseType::DeferredChannelMessageWithSource)
-          }),
-        )
-        .await;
+      let user_id = command.user.id;
+      let channel_id = command.channel_id;
+      let user_name = command.user.name.clone();
+      let user_channel_key = (user_id, channel_id);
 
-        match acknowledge_result {
-          Ok(Ok(_)) => {
-            debug!("Acknowledged the interaction");
-          }
-          Ok(Err(why)) => {
-            error!("Error acknowledging interaction: {:?}", why);
+      match command.data.name.as_str() {
+        "chat" => {
+          let input = command
+            .data
+            .options
+            .get(0)
+            .and_then(|opt| opt.value.as_ref())
+            .and_then(|value| value.as_str())
+            .unwrap_or("");
+
+          // Log the user's input with their name and ID
+          info!(
+            "User {}#{}: {}",
+            user_name, command.user.discriminator, input
+          );
+
+          // Generate AI response here
+          let api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not found");
+          let model = "gpt-3.5-turbo";
+
+          let chat_history = {
+            let mut chat_histories = self.chat_histories.lock().unwrap();
+            chat_histories
+              .entry(user_channel_key)
+              .or_insert_with(String::new)
+              .clone()
+          };
+
+          let ai_response =
+            generate_ai_response(input, model, &api_key, user_channel_key, chat_history).await;
+
+          // Send the follow-up message
+          if let Some(response) = ai_response.clone() {
+            if let Err(_) = create_followup_message(&ctx, &command, response).await {
+              return;
+            }
+          } else {
+            error!("Error: AI response could not be generated.");
             return;
           }
-          Err(_) => {
-            error!("Timed out acknowledging interaction");
-            return;
+
+          // Update the chat history with the user's input and the AI's response
+          {
+            let mut chat_histories = self.chat_histories.lock().unwrap();
+            let history = chat_histories
+              .entry(user_channel_key)
+              .or_insert_with(String::new);
+            history.push_str(&format!("User: {}\n", input));
+
+            if let Some(ref response) = ai_response {
+              history.push_str(&format!("AI: {}\n", response));
+            }
+
+            // Truncate the chat history to a certain number of tokens
+            let TOKEN_LIMIT = 4096;
+            // debug!("Chat history before truncation:\n{}", history);
+            truncate_chat_history(history, TOKEN_LIMIT);
+            // debug!("Chat history after truncation:\n{}", history);
           }
         }
 
-        // Get the user and channel IDs for this interaction
-        let user_id = command.user.id;
-        let channel_id = command.channel_id;
-        let user_name = command.user.name.clone();
-
-        // Log the user's input with their name and ID
-        info!("User {}#{}: {}", user_name, command.user.discriminator, input);
-
-        // Generate AI response here
-        let api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not found");
-        let model = "gpt-3.5-turbo";
-        let user_channel_key = (user_id, channel_id);
-
-        let chat_history = {
-          let mut chat_histories = self.chat_histories.lock().unwrap();
-          chat_histories
-            .entry(user_channel_key)
-            .or_insert_with(String::new)
-            .clone()
-        };
-
-        let ai_response =
-          generate_ai_response(input, model, &api_key, user_channel_key, chat_history).await;
-
-        // Update the chat history with the user's input and the AI's response
-        {
-          let mut chat_histories = self.chat_histories.lock().unwrap();
-          let history = chat_histories
-            .entry(user_channel_key)
-            .or_insert_with(String::new);
-          history.push_str(&format!("User: {}\n", input));
-          if let Some(ref response) = ai_response {
-            history.push_str(&format!("AI: {}\n", response));
+        "reset" => {
+          // Remove the chat history for the user who called the command
+          {
+            let mut chat_histories = self.chat_histories.lock().unwrap();
+            chat_histories.remove(&(user_id, channel_id));
           }
 
-          // Truncate the chat history to a certain number of tokens
-          let TOKEN_LIMIT = 4096;
-          // debug!("Chat history before truncation:\n{}", history);
-          truncate_chat_history(history, TOKEN_LIMIT);
-          // debug!("Chat history after truncation:\n{}", history);
+          // Send a follow-up message to indicate the chat history has been reset
+          let reset_message = "Chat history has been reset.".to_string();
+
+          if let Err(_) = create_followup_message(&ctx, &command, reset_message).await {
+            return;
+          }
         }
-
-        // Send the follow-up message
-        match command
-          .create_followup_message(&ctx.http, |message| {
-            message.content(
-              ai_response
-                .unwrap_or_else(|| "Error: AI response could not be generated.".to_string()),
-            );
-            message
-          })
-          .await
-        {
-          Ok(_) => {
-            debug!("Sent the follow-up message");
-          }
-          Err(why) => {
-            error!("Error sending follow-up message: {:?}", why);
-          }
+        _ => {
+          error!("Unknown command: {}", command.data.name);
         }
       }
+    }
+  }
+}
+
+async fn create_followup_message(
+  ctx: &Context,
+  command: &ApplicationCommandInteraction,
+  content: String,
+) -> Result<(), ()> {
+  match command
+    .create_followup_message(&ctx.http, |message| {
+      message.content(content);
+      message
+    })
+    .await
+  {
+    Ok(_) => {
+      debug!("Sent the follow-up message");
+      Ok(())
+    }
+    Err(why) => {
+      error!("Error sending follow-up message: {:?}", why);
+      Err(())
+    }
+  }
+}
+
+async fn acknowledge_interaction(
+  command: &ApplicationCommandInteraction,
+  ctx: &Context,
+) -> Result<(), ()> {
+  match timeout(
+    Duration::from_millis(2500),
+    command.create_interaction_response(&ctx.http, |response| {
+      response.kind(InteractionResponseType::DeferredChannelMessageWithSource)
+    }),
+  )
+  .await
+  {
+    Ok(Ok(_)) => {
+      debug!("Acknowledged the interaction");
+      Ok(())
+    }
+    Ok(Err(why)) => {
+      error!("Error acknowledging interaction: {:?}", why);
+      Err(())
+    }
+    Err(_) => {
+      error!("Timed out acknowledging interaction");
+      Err(())
     }
   }
 }
@@ -291,27 +338,35 @@ async fn register_application_commands(http: &Http) -> Result<(), Box<dyn std::e
   let commands = http.get_global_application_commands().await?;
 
   // Define the commands to register, along with their name, description, and options
-  let commands_to_register = vec![("ask", "Your message to the AI", CommandOptionType::String)];
+  let commands_to_register = vec![
+    ("chat", "Your message to the AI", Some(CommandOptionType::String)),
+    (
+      "reset",
+      "Reset the chat history",
+      None,
+    ),
+  ];
 
   // Check if the commands already exist
-  for (name, description, options) in commands_to_register {
+  for (name, description, option_type) in commands_to_register {
     let command_exists = commands.iter().any(|c| c.name == *name);
 
     // If the command doesn't exist, create it and add it as a global command
     if !command_exists {
-      let command_result = Command::create_global_application_command(http, |command| {
-        command
-          .name(name)
-          .description(description)
-          .create_option(|option| {
-            option
-              .name(name)
-              .description(description)
-              .kind(options)
-              .required(true)
-          })
-      })
-      .await;
+			let command_result = Command::create_global_application_command(http, |command| {
+					command.name(name).description(description);
+					if let Some(options) = option_type {
+							command.create_option(|option| {
+									option
+											.name(name)
+											.description(description)
+											.kind(options)
+											.required(true)
+							});
+					}
+					command
+			})
+			.await;
 
       // Log the result of registering the command
       match command_result {
